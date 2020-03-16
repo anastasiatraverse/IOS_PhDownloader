aca3e05952ce2ca807454d712170e3b5f05e4b94
Anastasia <traverse@ucu.edu.ua>
2020-03-16 14:18:37 +0200

    add basetabcontroller

--- PhDownloader/PhDownloader/Controller/BaseTabBarViewController.swift
+++ PhDownloader/PhDownloader/Controller/BaseTabBarViewController.swift
@@ -1,14 +1,14 @@
 //
-//  InProgressViewController.swift
+//  BaseTabBarViewController.swift
 //  PhDownloader
 //
-//  Created by Анастасия Траверсе.
+//  Created by Анастасия Траверсе on 16.03.2020.
 //  Copyright © 2020 Анастасия Траверсе. All rights reserved.
 //
 
 import UIKit
 
-class InProgressViewController: UIViewController {
+class BaseTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
